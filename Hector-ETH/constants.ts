3e751a139e2b1d5afad47672aa308e5715283775
sameretto <samer603m@yahoo.com>
2022-02-28 21:34:44 -0500

    added illuvium and matic to eth graphs

--- Hector-ETH/constants.ts
+++ Hector-ETH/constants.ts
@@ -6,4 +6,6 @@ export const ETH_WALLET_ADDRESS = "0x4bfb33d65f4167EBE190145939479227E7bf2CB0";
 export const MATIC_ADDRESS = "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0";
 export const UNISWAP_V3_POOL = "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8";
 export const MATIC_USDT_PAIR = "0x68f73e2180024db5b54e0e119d4f5128953f9417";
+export const IllUV_ETH_PAIR = "0xbaec0e18c770993ffb1175fef493b5113cc6e32d";
+export const ETH_USDT_PAIR = "0x11b815efb8f581194ae79006d24e0d814b7697f6";
 export const ILLUVIUM_ADDRESS = "0x767fe9edc9e0df98e07454847909b5e959d7ca0e";
--- Hector-ETH/src/EthMetrics.ts
+++ Hector-ETH/src/EthMetrics.ts
@@ -6,10 +6,15 @@ import { Illuvium } from '../generated/EthMetrics/Illuvium';
 import { UniswapV3Factory } from '../generated/EthMetrics/UniswapV3Factory';
 import { UniswapV3Pool } from '../generated/EthMetrics/UniswapV3Pool';
 import { Address, BigDecimal, BigInt, log } from '@graphprotocol/graph-ts';
-import { BASE_REWARD_POOL_ADDRESS, CONVEX_ALLOCATOR_ADDRESS, ETH_WALLET_ADDRESS, ILLUVIUM_ADDRESS, MATIC_ADDRESS, MATIC_USDT_PAIR, WORMHOLE_ADDRESS } from '../constants';
+import { BASE_REWARD_POOL_ADDRESS, CONVEX_ALLOCATOR_ADDRESS, ETH_USDT_PAIR, ETH_WALLET_ADDRESS, ILLUVIUM_ADDRESS, IllUV_ETH_PAIR, MATIC_ADDRESS, MATIC_USDT_PAIR, WORMHOLE_ADDRESS } from '../constants';
 
 const DEFAULT_DECIMALS = 18;
-
+const FixedPoint_Q96_BD = BigInt.fromI32(2).pow(96).toBigDecimal();
+const ZERO_BI = BigInt.fromI32(0);
+const ONE_BI = BigInt.fromI32(1);
+const ZERO_BD = BigDecimal.fromString('0');
+const BI_18 = BigInt.fromI32(18);
+const BI_6 = BigInt.fromI32(6);
 
 export function toDecimal(
     value: BigInt,
@@ -47,20 +52,54 @@ export function getBaseRewardPool(): BigDecimal {
 function getTreasuryMaticValue(): BigDecimal {
     const maticContract = Matic.bind(Address.fromString(MATIC_ADDRESS));
     const maticBalance = toDecimal(maticContract.balanceOf(Address.fromString(ETH_WALLET_ADDRESS)), 18);
-
-    // const pair = UniswapV3Pool.bind(Address.fromString(MATIC_USDT_PAIR));
-    // const pool = new Pool(MATIC_USDT_PAIR);
-    // log.debug('token0 Price {}', [pool.token0Price.toString()]);
-    // log.debug('token1 Price {}', [pool.token1Price.toString()]);
-
-    // const poolDay = new PoolDayData(MATIC_USDT_PAIR);
-    // log.debug('poolDay token0 Price {}', [poolDay.token0Price.toString()]);
-    // log.debug('poolDay token1 Price {}', [poolDay.token1Price.toString()]);
-    return maticBalance;
+    log.debug('Matic Balance {}', [maticBalance.toString()])
+    const pair = UniswapV3Pool.bind(Address.fromString(MATIC_USDT_PAIR));
+    const result = pair.try_slot0();
+    if (!result.reverted) {
+        let sqrtPrice = (result.value.value0).toBigDecimal().div(FixedPoint_Q96_BD);
+        let maticPrice = sqrtPrice.times(sqrtPrice)
+            .times(exponentToBigDecimal(BI_18))
+            .div(exponentToBigDecimal(BI_6));
+        return maticPrice.times(maticBalance);
+    } else {
+        return ZERO_BD;
+    }
 }
+
+export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
+    let bd = BigDecimal.fromString('1');
+    for (let i = ZERO_BI; i.lt(decimals); i = i.plus(ONE_BI)) {
+        bd = bd.times(BigDecimal.fromString('10'))
+    }
+    return bd;
+}
+
 
 function getTreasuryIlluviumValue(): BigDecimal {
     const illuviumContract = Illuvium.bind(Address.fromString(ILLUVIUM_ADDRESS));
     const illuviumBalance = toDecimal(illuviumContract.balanceOf(Address.fromString(ETH_WALLET_ADDRESS)), 18);
-    return illuviumBalance;
+    const pair = UniswapV3Pool.bind(Address.fromString(IllUV_ETH_PAIR));
+    const result = pair.try_slot0();
+    if (!result.reverted) {
+        const sqrtPrice = (result.value.value0).toBigDecimal().div(FixedPoint_Q96_BD);
+        const ilvPerETH = sqrtPrice.times(sqrtPrice)
+            .times(exponentToBigDecimal(BI_18))
+            .div(exponentToBigDecimal(BI_18));
+
+        const ethUSDTPair = UniswapV3Pool.bind(Address.fromString(ETH_USDT_PAIR));
+        const res = ethUSDTPair.try_slot0();
+        if (!res.reverted) {
+            const ethUSDTSqrtPrice = (res.value.value0).toBigDecimal().div(FixedPoint_Q96_BD);
+            const ethUSDTPrice = ethUSDTSqrtPrice.times(ethUSDTSqrtPrice)
+                .times(exponentToBigDecimal(BI_18))
+                .div(exponentToBigDecimal(BI_6));
+            const illuviumUsdPrice = ethUSDTPrice.times(ilvPerETH);
+            log.debug('illuvium USD PRICE {}', [illuviumUsdPrice.toString()]);
+            return illuviumUsdPrice.times(illuviumBalance);
+        } else {
+            return ZERO_BD;
+        }
+    } else {
+        return ZERO_BD;
+    }
 }
