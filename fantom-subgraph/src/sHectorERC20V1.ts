44b69cdd03acdd2dfb50d1310444626ecc5980aa
sameretto <samer603m@yahoo.com>
2022-03-28 15:22:23 -0400

    fixed dividing by 0 issue

--- fantom-subgraph/src/sHectorERC20V1.ts
+++ fantom-subgraph/src/sHectorERC20V1.ts
@@ -1,10 +1,10 @@
 import { RebaseCall } from '../generated/sHectorERC20V1/sHectorERC20'
 import { HectorERC20 } from '../generated/sHectorERC20V1/HectorERC20'
 import { Rebase } from '../generated/schema'
-import { Address, BigInt, log } from '@graphprotocol/graph-ts'
-import {HEC_ERC20_CONTRACT, STAKING_CONTRACT_V1} from './utils/Constants'
+import { Address, BigDecimal, BigInt, log } from '@graphprotocol/graph-ts'
+import { HEC_ERC20_CONTRACT, STAKING_CONTRACT_V1 } from './utils/Constants'
 import { toDecimal } from './utils/Decimals'
-import {getHECUSDRate} from './utils/Price';
+import { getHECUSDRate } from './utils/Price';
 
 export function rebaseFunction(call: RebaseCall): void {
     let rebaseId = call.transaction.hash.toHex()
@@ -18,7 +18,9 @@ export function rebaseFunction(call: RebaseCall): void {
         rebase.amount = toDecimal(call.inputs.profit_, 9)
         rebase.stakedHecs = toDecimal(hec_contract.balanceOf(Address.fromString(STAKING_CONTRACT_V1)), 9)
         rebase.contract = STAKING_CONTRACT_V1
-        rebase.percentage = rebase.amount.div(rebase.stakedHecs)
+        if (rebase.stakedHecs.notEqual(BigDecimal.fromString('0'))) {
+            rebase.percentage = rebase.amount.div(rebase.stakedHecs)
+        }
         rebase.transaction = rebaseId
         rebase.timestamp = call.block.timestamp
         rebase.value = rebase.amount.times(getHECUSDRate())
